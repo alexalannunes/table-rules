965393b27d53e3089e73e2af386e6214ee1a769f
Alex Alan Nunes <alexalannunes@gmail.com>
2025-04-03 16:09:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+
+function App() {
+  return (
+    <main className="bg-background min-h-screen">
+      <Button className="purple:bg-red-600">Helo</Button>
+      <Input />
+    </main>
+  );
+}
+
+export default App;
